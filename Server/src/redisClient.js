e9dc1efb9beff1dc17663403ad206b4800b35c02
lokeshreddy29 <lokeshr3ddy@gmail.com>
2025-12-31 12:24:39 +0530

    changes for local deploy

--- Server/src/redisClient.js
+++ Server/src/redisClient.js
@@ -8,4 +8,7 @@ client.on('error', err => console.log('Redis Client Error', err));
 
 await client.connect();
 
+const key = await client.get('mykey')
+if(!key) await client.set('mykey', 0)
+
 export default client
